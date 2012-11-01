5946a30daaf2f08300299ecf8d85ee6ab50b5088
bspkrs <bspkrs@gmail.com>
2012-11-01 14:18:08 -0400

    fixed tick registration - 1.4.2.r02 commit

--- net/minecraft/src/mod_crystalWing.java
+++ net/minecraft/src/mod_crystalWing.java
@@ -17,7 +17,7 @@ public class mod_crystalWing extends BaseMod
     public static int uses = 8;
     @MLProp(info="Maximum distance for the Burned Wing teleportation.\n\n**ONLY EDIT WHAT IS BELOW THIS**")
     public static int teleDistance = 500;
-    
+
     static int indexCrystalWing = ModLoader.addOverride("/gui/items.png", "/daftpvf/crystalWing.png");
     static int indexBurningWing = ModLoader.addOverride("/gui/items.png", "/daftpvf/crystalWingBurning.png");
     static int indexBurnedWing = ModLoader.addOverride("/gui/items.png", "/daftpvf/crystalWingBurned.png");
@@ -25,7 +25,7 @@ public class mod_crystalWing extends BaseMod
     public static final Item crystalWingBurning = (new ItemCrystalWingBurning(idBurningWing - 256)).setItemName("burningWing").setIconIndex(indexBurningWing);
     public static final Item crystalWingBurned = (new ItemCrystalWingBurned(idBurnedWing - 256, teleDistance)).setItemName("burnedWing").setIconIndex(indexBurnedWing);
     public static final Achievement burnedWing = (new Achievement(1710, "burnedWing", 9, -5, crystalWingBurning, null)).registerAchievement();
-    
+
     private boolean checkUpdate;
     private ModVersionChecker versionChecker;
     private String versionURL = "https://dl.dropbox.com/u/20748481/Minecraft/1.4.2/crystalWing.version";
@@ -33,6 +33,11 @@ public class mod_crystalWing extends BaseMod
 
     public mod_crystalWing()
     {
+        if(uses > 0)
+        {
+            crystalWing.setMaxDamage(uses - 1);
+        }
+        
         ModLoader.addName(crystalWing, "Crystal Wing");
         ModLoader.addName(crystalWingBurning, "BURNING WING");
         ModLoader.addName(crystalWingBurned, "Burned Wing");
@@ -40,28 +45,24 @@ public class mod_crystalWing extends BaseMod
         ModLoader.addRecipe(new ItemStack(crystalWing, 1), new Object[] {
             "GGG", "EFF", Character.valueOf('G'), Item.ingotGold, Character.valueOf('E'), Item.enderPearl, Character.valueOf('F'), Item.feather
         });
-        if(uses > 0)
-        {
-            crystalWing.setMaxDamage(uses - 1);
-        }
-        ModLoader.setInGameHook(this, true, true);
-        
+
         checkUpdate = allowUpdateCheck;
         versionChecker = new ModVersionChecker(getName(), getVersion(), versionURL, mcfTopic, ModLoader.getLogger());
     }
 
     public String getVersion()
     {
-        return "ML 1.4.2.r01";
+        return "ML 1.4.2.r02";
     }
 
     public void load()
     {
         versionChecker.checkVersionWithLogging();
+        ModLoader.setInGameHook(this, true, true);
     }
-    
+
     public boolean onTickInGame(float f, Minecraft mc)
-    {        
+    {
         if(checkUpdate)
         {
             if(!versionChecker.isCurrentVersion())
